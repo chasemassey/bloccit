c9d929be0c066ea0e7bf88435b1a829eb7e325ad
CharlesMassey <chasemassey@gmail.com>
2015-12-09 21:35:28 -0500

    added seeds

--- db/migrate/20151210014404_create_sponsored_posts.rb
+++ db/migrate/20151210014404_create_sponsored_posts.rb
@@ -4,7 +4,6 @@ class CreateSponsoredPosts < ActiveRecord::Migration
       t.string :title
       t.text :body
       t.integer :price
-      t.references :topic, index:true, foreign_key:true
 
       t.timestamps null: false
     end
